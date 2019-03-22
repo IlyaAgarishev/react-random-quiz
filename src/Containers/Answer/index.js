1adf9643bc9abe75fb578f9837e62585550a5096
IlyaAgarishev <ilyagarishev@gmail.com>
2019-03-22 08:53:17 +0500

    anwer code washing

--- src/Containers/Answer/index.js
+++ src/Containers/Answer/index.js
@@ -1,40 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import "./radio.css";
+import styles from "./index.module.css";
+import PropTypes from "prop-types";
 
-class Answer extends React.Component {
-  render() {
-    return (
-      <div
-        className="answer-wrapper"
-        ref={ref => {
-          this.answerWrapper = ref;
+export const Answer = props => {
+  const { answerId, answer, onInputChange } = { ...props };
+  return (
+    <div className={styles.answerWrapper}>
+      <input
+        type="radio"
+        name="answer"
+        id={answerId}
+        onChange={() => {
+          onInputChange(answer);
         }}
-      >
-        <input
-          type="radio"
-          name="answer"
-          id={this.props.answerId}
-          className="answer"
-          answer={this.props.answer}
-          onChange={() => {
-            let answer = this.answerLabel.innerHTML;
-            this.props.onInputChange(answer);
-          }}
-        />
-        <label
-          ref={ref => {
-            this.answerLabel = ref;
-          }}
-          id={this.props.answerId}
-          htmlFor={this.props.answerId}
-          className="answerWord"
-        >
-          {this.props.answer}
-        </label>
-      </div>
-    );
-  }
-}
+      />
+      <label id={answerId} htmlFor={answerId} className={styles.answerWord}>
+        {answer}
+      </label>
+    </div>
+  );
+};
+
+Answer.propTypes = {
+  answerId: PropTypes.string.isRequired,
+  answer: PropTypes.string.isRequired,
+  onInputChange: PropTypes.func.isRequired
+};
 
 export default Answer;
