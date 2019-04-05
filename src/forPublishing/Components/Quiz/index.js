b72c90bd032074d54aad861968a5b9f6ba77a838
IlyaAgarishev <ilyagarishev@gmail.com>
2019-04-05 16:56:50 +0500

    some changes

--- src/forPublishing/Components/Quiz/index.js
+++ src/forPublishing/Components/Quiz/index.js
@@ -49,77 +49,74 @@ class Quiz extends React.Component {
 
   render() {
     return (
-      <div className={styles.quizWrapper}>
+      <div
+        className={setQuizBackground(this.state.background, styles)}
+        ref={ref => {
+          this.quiz = ref;
+        }}
+      >
+        <div className={styles.questionText}>
+          "{this.state.questionWord}" переводится как ?
+        </div>
+        <form>
+          {this.state.answersArray.map((element, index) => {
+            return (
+              <Answer
+                onInputChange={this.onInputChange}
+                answer={element.answer}
+                key={index}
+                answerId={uniqueIdGenerator(
+                  this.state.questionWord,
+                  element.answer,
+                  index
+                )}
+              />
+            );
+          })}
+        </form>
         <div
-          className={setQuizBackground(this.state.background, styles)}
-          ref={ref => {
-            this.quiz = ref;
+          className={styles.checkAnswer}
+          onClick={() => {
+            if (this.state.selectedAnswer === this.state.rightAnswer) {
+              this.setState({ background: "green" });
+              setTimeout(() => {
+                this.setState({
+                  questionIndex: this.state.questionIndex + 1
+                });
+                if (
+                  this.state.questionIndex !== this.state.wordsToTest.length
+                ) {
+                  this.setState({
+                    questionWord: this.state.wordsToTest[
+                      this.state.questionIndex
+                    ].word,
+                    rightAnswer: this.state.wordsToTest[
+                      this.state.questionIndex
+                    ].translation.toLowerCase()
+                  });
+                  finalAnswersArrayGenerator(
+                    this.state.rightAnswer,
+                    this.setAnswersArray
+                  );
+                  this.setState({ background: "white" });
+                  uncheckRadioInputs(this.quiz.children[1].children);
+                } else {
+                  if (this.props.clearDictionary) this.props.clearDictionary();
+                }
+              }, 700);
+            } else {
+              this.setState({ background: "red" });
+              uncheckRadioInputs(this.quiz.children[1].children);
+              setTimeout(() => {
+                this.setState({ background: "white" });
+              }, 150);
+            }
           }}
         >
-          <div className={styles.questionText}>
-            "{this.state.questionWord}" переводится как ?
-          </div>
-          <form>
-            {this.state.answersArray.map((element, index) => {
-              return (
-                <Answer
-                  onInputChange={this.onInputChange}
-                  answer={element.answer}
-                  key={index}
-                  answerId={uniqueIdGenerator(
-                    this.state.questionWord,
-                    element.answer,
-                    index
-                  )}
-                />
-              );
-            })}
-          </form>
-          <div
-            className={styles.checkAnswer}
-            onClick={() => {
-              if (this.state.selectedAnswer === this.state.rightAnswer) {
-                this.setState({ background: "green" });
-                setTimeout(() => {
-                  this.setState({
-                    questionIndex: this.state.questionIndex + 1
-                  });
-                  if (
-                    this.state.questionIndex !== this.state.wordsToTest.length
-                  ) {
-                    this.setState({
-                      questionWord: this.state.wordsToTest[
-                        this.state.questionIndex
-                      ].word,
-                      rightAnswer: this.state.wordsToTest[
-                        this.state.questionIndex
-                      ].translation.toLowerCase()
-                    });
-                    finalAnswersArrayGenerator(
-                      this.state.rightAnswer,
-                      this.setAnswersArray
-                    );
-                    this.setState({ background: "white" });
-                    uncheckRadioInputs(this.quiz.children[1].children);
-                  } else {
-                    if (this.props.clearDictionary)
-                      this.props.clearDictionary();
-                  }
-                }, 700);
-              } else {
-                this.setState({ background: "red" });
-                uncheckRadioInputs(this.quiz.children[1].children);
-                setTimeout(() => {
-                  this.setState({ background: "white" });
-                }, 150);
-              }
-            }}
-          >
-            Дальше
-          </div>
-          <div className={styles.questionsCounter}>
-            {this.state.questionIndex + 1} / {this.state.wordsToTest.length}
-          </div>
+          Дальше
+        </div>
+        <div className={styles.questionsCounter}>
+          {this.state.questionIndex + 1} / {this.state.wordsToTest.length}
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import "./index.css";
 import Quiz from "./forPublishing";
 
 ReactDOM.render(
-  <Quiz
-    wordsToTest={[
-      { word: "Wolf", translation: "Волк" },
-      { word: "Sad", translation: "Грустный" },
-      { word: "Cat", translation: "Кот" },
-      { word: "Dog", translation: "Собака" }
-    ]}
-  />,
+  <div className="qiqi">
+    <Quiz
+      wordsToTest={[
+        { word: "Wolf", translation: "Волк" },
+        { word: "Sad", translation: "Грустный" },
+        { word: "Cat", translation: "Кот" },
+        { word: "Dog", translation: "Собака" }
+      ]}
+    />
+  </div>,
   document.getElementById("root")
 );
